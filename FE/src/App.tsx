14fd53d89d50014e3c10debd96fcc4c1b7e28b9c
정주은 <jooeun0807@naver.com>
2025-03-17 16:47:53 +0900

    [FE] feat: 라우트 주소 설정
    라우트 주소 설정 구현
    
    ref: S12P21D203-27

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,5 +1,41 @@
-const App = () => {
-  return 'Hello World';
-};
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Result from './pages/Result';
+import Dictionary from './pages/Dictionary';
+import Quiz from './pages/Quiz';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />, // 공통 레이아웃
+    children: [
+      {
+        // 홈
+        index: true, // 기본 경로 (/)
+        element: <Home />,
+      },
+      {
+        // 검색 결과
+        path: 'result',
+        element: <Result />,
+      },
+      {
+        // 딕셔너리
+        path: 'dictionary',
+        element: <Dictionary />,
+      },
+      {
+        // 퀴즈
+        path: 'quiz',
+        element: <Quiz />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
 
 export default App;
