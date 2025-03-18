ede28ec22a2ee472d0b10f21caaf6a9df95bce4b
윤지원 <dusqha6731@naver.com>
2025-03-18 23:35:41 +0900

    [FE] feat: Home -> Dict/Quiz 페이지 연결
    버튼 생성 및 주요 페이지 이동 기능 구현
    
    ref: S12P21D203-123
    related to: S12P21D203-4, S12P21D203-44

--- FE/src/pages/home/Home.tsx
+++ FE/src/pages/home/Home.tsx
@@ -1,4 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+
 function Home() {
+  const navigate = useNavigate();
+  
+  const handleDictionaryClick = () => {
+    // Dictionary로 이동
+    navigate('/dictionary');
+  };
+  
+  const handleQuizClick = () => {
+    // Quiz로 이동
+    navigate('/quiz');
+  };
+
   return (
     <div
       className="min-h-screen w-full flex flex-col"
@@ -42,7 +56,7 @@ function Home() {
               <div className="bg-indigo-500 w-2 lg:w-3"></div>
               <div
                 className="bg-indigo-500 rounded-r-lg 
-                w-12 h-6 md:w-14 md:h-7 lg:w-16 lg:h-8"
+                w-10 h-5 md:w-14 md:h-7 lg:w-16 lg:h-8"
               ></div>
             </div>
           </div>
@@ -50,6 +64,43 @@ function Home() {
         <p className="text-lg md:text-xl lg:text-2xl mt-2 text-center font-[NanumSquareRound]">
           의미있는 제스처를 통해 세상을 열어보세요
         </p>
+
+        {/* 버튼 영역 */}
+        <div className="flex justify-center items-center mt-20 space-x-8">
+          <button 
+            onClick={handleDictionaryClick}
+            className="flex flex-col items-center bg-transparent border-none cursor-pointer"
+          >
+            <div 
+            className="flex items-center justify-center relative
+            w-50 h-20 md:w-80 md:h-30 lg:w-90 lg:h-40 
+            bg-inch-worm-500 rounded-full"
+            >
+              <div className="w-48 h-40 relative">
+                {/* 책 이미지 - 간단한 구현 */}
+        
+              </div>
+            </div>
+            <span className="mt-4 text-2xl font-bold">Dictionary</span>
+          </button>
+          
+          <button 
+            onClick={handleQuizClick}
+            className="flex flex-col items-center bg-transparent border-none cursor-pointer"
+          >
+            <div 
+            className="flex items-center justify-center relative
+            w-50 h-20 md:w-80 md:h-30 lg:w-90 lg:h-40 
+            bg-lavender-rose-300 rounded-full"
+            >
+              <div className="w-48 h-40 relative">
+                {/* 퀴즈 노트 및 필기구 이미지 */}
+                
+              </div>
+            </div>
+            <span className="mt-4 text-2xl font-bold">Quiz</span>
+          </button>
+        </div>
       </div>
     </div>
   );
