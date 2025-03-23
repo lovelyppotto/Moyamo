e01beb639e43464c6671f2d9bb0cd0648c09ebb8
joehyejeong <gpwjdwngml@naver.com>
2025-03-24 04:32:09 +0900

    feat: AI 이외 퀴즈 페이지 제작 - 제스처 퀴즈 페이지 UI - 의미 퀴즈 페이지 UI
    resolves: S12P21D203-139
    related to: S12P21D203-138, S12P21D203-136, S12P21D203-137

--- FE/src/pages/quiz/gesture_quiz/GestureQuiz.tsx
+++ FE/src/pages/quiz/gesture_quiz/GestureQuiz.tsx
@@ -1,32 +1,62 @@
 // import '../../index.css';
-import { faDoorOpen, faVolumeXmark } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-import QuizProgress from '../Progress';
+import Progress from '@/pages/quiz/Progress';
 
 function GestureQuiz() {
   return (
     <>
-      {/* 헤더부분 */}
-      <div className="pb-1 flex justify-between items-center py-3">
-        <button>
-          <FontAwesomeIcon icon={faDoorOpen} />
-        </button>
-        <div className="flex justfy-center items-conter">
-          {/* 화면에 들어왔을 때부터 초가 줄어드는 함수 만들기 */}
-          <p className="absolute z-10 transform translate-x-2 translate-y-0.5 text-[15px] ">60</p>
-          <img src="/images/Time.png" alt="Timer" className="z-1 w-8 h-8" />
+    
+        <div className='flex flex-col h-screen overflow-hidden w-full'>
+          {/* 초시계: 디자인 더 이쁜 시계로 바꾸기! */}
+          <div className="absolute left-1/2 top-4 transform -translate-x-1/2 z-10">
+            <div className="flex justfy-center items-conter">
+              {/* 퀴즈 화면에 들어왔을 때부터 초가 줄어드는 부분: 함수 만들기 => progress component로 옮기는것 고려하기기*/}
+              <p className="absolute z-10 transform translate-x-3 translate-y-3 text-2xl font-[NanumSquareRoundB] text-gray-400">10</p>
+              <img src="/images/Time.png" alt="Timer" className="z-1 w-15 h-15" />
+            </div>
+          </div>
+          {/* 진행 바, 퀴즈박스 */}
+          {/* progress바를 seekbar로 교체하기!!! */}
+          {/* 마진 탑(margin-top)을 뷰포트 높이(viewport height)의 5%로 설정 */}
+          <div className='h-screen flex flex-col mt-[10vh] mx-[10vh]'>
+            <Progress/>
+            <div className='flex justify-between items-center mt-[3vh]'>
+              <h1 className='text-2xl lg:text-3xl 2xl:text-4xl font-[NanumSquareRoundB]'>Q.이 손 동작이 의미하는 것은 무엇일까요?</h1>
+              <button className='flex justify-between items-center rounded-2xl py-1 px-3 hover:bg-gray-200'>
+                <p className='text-xl 2xl:text-2xl font-[NanumSquareRoundB]'>Skip</p>
+                <FontAwesomeIcon icon={faArrowRight} className='m-3'/>
+              </button>
+            </div>
+            <div className="flex justify-center w-full h-2/5 bg-white rounded-xl drop-shadow-quiz-box  my-[3vh]">
+              {/* 추후, 백앤드에서 blender 애니메이션을 가져올 예정 */}
+              <img src="/images/gesture_example.png" alt="sample_img" className='p-5'/>
+            </div>
+             {/* 퀴즈 보기 부분 */}
+             {/* 퀴즈 내용 백앤드에서 받아와서 보여지도록 하기 */}
+             {/* 폰트어썸 유료 결제하면 icon circle_1이걸로 바꾸기! */}
+            <div className='flex justify-around mt-[3vh]'>
+              <button className=' flex justify-center items-center w-2/5 h-[10vh] bg-white rounded-xl drop-shadow-quiz-box hover:bg-[var(--color-kr-500)] hover:text-white text-3xl lg:text-4xl font-[NanumSquareRoundB]'>
+                <p className='mr-5'>①</p>
+                <p>사랑해</p>
+              </button>
+              <button className=' flex justify-center items-center w-2/5 h-[10vh] bg-white rounded-xl drop-shadow-quiz-box hover:bg-[var(--color-kr-500)] hover:text-white text-3xl lg:text-4xl font-[NanumSquareRoundB]'>
+                <p className='mr-5'>②</p>
+                <p>미안해</p>
+              </button>
+            </div>
+            <div className='flex justify-around mt-[3vh]'>
+            <button className=' flex justify-center items-center w-2/5 h-[10vh] bg-white rounded-xl drop-shadow-quiz-box hover:bg-[var(--color-kr-500)] hover:text-white text-3xl lg:text-4xl font-[NanumSquareRoundB]'>
+                <p className='mr-5'>③</p>
+                <p>승리</p>
+              </button>              <button className=' flex justify-center items-center w-2/5 h-[10vh] bg-white rounded-xl drop-shadow-quiz-box hover:bg-[var(--color-kr-500)] hover:text-white text-3xl lg:text-4xl font-[NanumSquareRoundB]'>
+                <p className='mr-5'>④</p>
+                <p>약속</p>
+              </button>
+            </div>
+          </div>
         </div>
-        {/* icon을 volume-xmark로 바꾸기 */}
-        <button>
-          <FontAwesomeIcon icon={faVolumeXmark} />
-        </button>
-      </div>
-      <div>
-        <QuizProgress value={13} className="w-3/5" />
-      </div>
-      <div className="w-full h-50 bg-amber-400 rounded-xl "></div>
-      <div></div>
+
     </>
   );
 }
--- FE/src/pages/quiz/meaning_quiz/MeaningQuiz.tsx
+++ FE/src/pages/quiz/meaning_quiz/MeaningQuiz.tsx
@@ -1,6 +1,64 @@
+// import '../../index.css';
+import { faArrowRight} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Progress from '@/pages/quiz/Progress';
+
 function MeaningQuiz() {
-  // 정적 디자인만 구현
-  return <div>하이</div>;
+  return (
+    <>
+      <div className='flex flex-col h-screen overflow-hidden w-full'>
+        {/* 초시계: 디자인 더 이쁜 시계로 바꾸기! */}
+        <div className="absolute left-1/2 top-4 transform -translate-x-1/2 z-10">
+          <div className="flex justfy-center items-conter">
+            {/* 퀴즈 화면에 들어왔을 때부터 초가 줄어드는 부분: 함수 만들기 => progress component로 옮기는것 고려하기기*/}
+            <p className="absolute z-10 transform translate-x-3 translate-y-3 text-2xl font-[NanumSquareRoundB] text-gray-400">10</p>
+            <img src="/images/Time.png" alt="Timer" className="z-1 w-15 h-15" />
+          </div>
+        </div>
+        {/* 진행 바, 퀴즈박스 */}
+        {/* progress바를 seekbar로 교체하기!!! */}
+        {/* 마진 탑(margin-top)을 뷰포트 높이(viewport height)의 5%로 설정 */}
+        <div className='h-screen flex flex-col mt-[10vh] mx-[10vh]'>
+          <Progress/>
+          <div className='flex justify-between items-center mt-[3vh]'>
+            <h1 className='text-2xl lg:text-3xl 2xl:text-4xl font-[NanumSquareRoundB]'>Q.이 상황에 적절한 제스처는 무엇인가요?</h1>
+            <button className='flex justify-between items-center rounded-2xl py-1 px-3 hover:bg-gray-200'>
+              <p className='text-xl 2xl:text-2xl font-[NanumSquareRoundB]'>Skip</p>
+              <FontAwesomeIcon icon={faArrowRight} className='m-3'/>
+            </button>
+          </div>
+          <div className="flex justify-center items-center w-full min-h-[16.666vh]  bg-white rounded-xl drop-shadow-quiz-box  my-[3vh]">
+            {/* 추후, 백앤드에서 제스처에 대한 설명을 가져올 예정 */}
+            {/* min-h와 break-all 를 사용하여, 글의 길이에 따라 공간이 생기게 함 */}
+            <p className='break-all p-3 text-2xl xl:text-3xl 2xl:text-4xl font-[NanumSquareRound]'>한국에서 이 동작은 사랑한다는 의미를 지닌다. 사진을 찍을 때에도 자주 사용한다. </p>
+          </div>
+            {/* 퀴즈 보기 부분 */}
+            {/* blender 애니메이션 백앤드에서 받아와서 보여지도록 하기 */}
+            {/* 폰트어썸 유료 결제하면 icon circle_1이걸로 바꾸기! */}
+          <div className='flex justify-around mt-[3vh]'>
+            <button className=' flex justify-center items-center w-2/5 h-[15vh] bg-white rounded-xl drop-shadow-quiz-box hover:bg-[var(--color-kr-500)] hover:text-white text-3xl lg:text-4xl font-[NanumSquareRoundB] '>
+              <p className='mr-5'>①</p>
+              <img src="/images/gesture_example.png" alt="sample_img" className='w-[10vh] p-2'/>
+            </button>
+            <button className=' flex justify-center items-center w-2/5 h-[15vh] bg-white rounded-xl drop-shadow-quiz-box hover:bg-[var(--color-kr-500)] hover:text-white text-3xl lg:text-4xl font-[NanumSquareRoundB]'>
+              <p className='mr-5'>②</p>
+              <img src="/images/gesture_example.png" alt="sample_img" className='w-[10vh] p-2'/>
+            </button>
+          </div>
+          <div className='flex justify-around mt-[3vh]'>
+          <button className=' flex justify-center items-center w-2/5 h-[15vh] bg-white rounded-xl drop-shadow-quiz-box hover:bg-[var(--color-kr-500)] hover:text-white text-3xl lg:text-4xl font-[NanumSquareRoundB]'>
+              <p className='mr-5'>③</p>
+              <img src="/images/gesture_example.png" alt="sample_img" className='w-[10vh] p-2'/>
+            </button>              <button className=' flex justify-center items-center w-2/5 h-[15vh] bg-white rounded-xl drop-shadow-quiz-box hover:bg-[var(--color-kr-500)] hover:text-white text-3xl lg:text-4xl font-[NanumSquareRoundB]'>
+              <p className='mr-5'>④</p>
+              <img src="/images/gesture_example.png" alt="sample_img" className='w-[10vh] p-2'/>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default MeaningQuiz;
+
