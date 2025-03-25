c12588e09b89c48720dcb84c1f5533eb4c25e83d
윤지원 <dusqha6731@naver.com>
2025-03-25 17:05:08 +0900

    [FE] design: 검색결과 다크모드 구현
    검색결과 다크모드 적용 완료
    
    ref: S12P21D203-179
    related to: S12P21D203-123

--- FE/src/pages/result/SearchResultItem.tsx
+++ FE/src/pages/result/SearchResultItem.tsx
@@ -33,17 +33,24 @@ function SearchResultItem({ result, onClick }: SearchResultItemProps) {
         </div>
         {/* 제스처 설명 - 말풍선 스타일 */}
         <div className="flex-1 ml-4 relative">
-          <div className="relative bg-white rounded-lg shadow-md p-6 dark:bg-gray-100 drop-shadow-basic">
+          <div 
+            className="relative rounded-lg shadow-md p-6
+            bg-white
+            dark:bg-gray-700 drop-shadow-basic
+            ">
             {/* 말풍선 꼬리 */}
             <div className="absolute left-0 top-2/3 transform -translate-x-full -translate-y-1/2">
               <svg width="15" height="30" viewBox="0 0 15 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M15 5 L0 15 L15 25 Z" fill="white" className="dark:fill-gray-100"/>
+                <path d="M15 5 L0 15 L15 25 Z" fill="white" className="dark:fill-gray-700"/>
               </svg>
             </div>
             
             {/* 제목 */}
             <div className="flex justify-between items-start mb-1 relative z-10">
-              <h3 className="text-3xl font-bold font-[NanumSquareRoundEB] text-gray-900">
+              <h3 
+                className="text-3xl font-bold font-[NanumSquareRoundEB] text-gray-900
+                dark:text-d-txt-50"
+              >
                 {result.gestureName}
               </h3>
               
@@ -64,7 +71,9 @@ function SearchResultItem({ result, onClick }: SearchResultItemProps) {
             </div>
             
             {/* 설명 텍스트 */}
-            <p className="text-lg text-gray-700 relative z-10 font-[NanumSquareRound]">
+            <p className="relative z-10 
+            text-lg text-gray-700 font-[NanumSquareRound]
+            dark:text-d-txt-50">
               {result.meanings[0]?.meaning}
             </p>
           </div>
