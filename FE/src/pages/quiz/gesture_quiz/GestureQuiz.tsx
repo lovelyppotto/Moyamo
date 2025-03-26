89e37d5622f3dd87c845c0e88abb0048037108d2
joehyejeong <gpwjdwngml@naver.com>
2025-03-26 14:51:37 +0900

    fix: progress bar 함수 수정 - progress가 초에 맞춰서 천천히 줄어들도록 수정 - 진행률을 0-100% 범위 내에서 관리하도록 수정
    resolves: S12P21D203-169
    related to : S12P21D203-168,S12P21D203-196, S12P21D203-167

--- FE/src/pages/quiz/gesture_quiz/GestureQuiz.tsx
+++ FE/src/pages/quiz/gesture_quiz/GestureQuiz.tsx
@@ -5,32 +5,29 @@ import Progress from '@/pages/quiz/Progress';
 import QuizResult from '../QuizResult';
 import PbNumber from '../PbNumber';
 import { useState, useCallback } from 'react';
-import QUSETIONS from '../questions.ts'
+import QUSETIONS from '../questions.ts';
 
 function GestureQuiz() {
   // const [isResult, setIsResult] = useState(false);
-  const [userAnswers, setUserAnswers] = useState([]);//유저가 선택한 답들
+  const [userAnswers, setUserAnswers] = useState([]); //유저가 선택한 답들
   const activeQuestionIndex = userAnswers.length; // 몇 번째 문제를 보여줄 지 index번호 (최소한의 useState로 최대한의 상태 가져오는 것: 최적화)
   // 유저에게 보여지는 답변을 섞기: 복사본을 만들어야(원본은 유지) 정답 여부를 확인할 수 있음.
   const quizIsComplete = activeQuestionIndex === QUSETIONS.length;
-  
-  //useCallback: 주변 컴포넌트 함수가 다시 실행되어도, 재생성되지 않음. 
-  //함수: 유저가 선택한 답을 배열에 추가함.
-  const handleSelectAnswer = useCallback(function handleSelectAnswer (selectedAnswer) {
-      setUserAnswers((prevUserAnswers)=>{
-        return [...prevUserAnswers, selectedAnswer];
-      });
-    }, []);
-  
 
-  const handleSkipAnswer = useCallback(() => 
-    ()=>handleSelectAnswer(null), [handleSelectAnswer]);
+  //useCallback: 주변 컴포넌트 함수가 다시 실행되어도, 재생성되지 않음.
+  //함수: 유저가 선택한 답을 배열에 추가함.
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }, []);
+
+  const handleSkipAnswer = useCallback(() => () => handleSelectAnswer(null), [handleSelectAnswer]);
 
   if (quizIsComplete) {
-    return(<QuizResult/>)
-    
+    return <QuizResult />;
   }
-  const shuffledAnswers = [...QUSETIONS[activeQuestionIndex].answers]; 
+  const shuffledAnswers = [...QUSETIONS[activeQuestionIndex].answers];
   shuffledAnswers.sort(() => Math.random() - 0.5); // 0~1미만의 값에서 0.5를 뺀다면 100가지 경우의 수 중 50개가 음수 (값이 섞임)
 
   return (
@@ -46,10 +43,14 @@ function GestureQuiz() {
         {/* progress바를 seekbar로 교체하기!!! */}
         {/* 마진 탑(margin-top)을 뷰포트 높이(viewport height)의 5%로 설정 */}
         <div className="h-screen flex flex-col mt-[5vh] xl:mt-[10vh] mx-[2vh] xl:mx-[10vh]">
-          <Progress timeout={10000} onTimeout={()=>handleSelectAnswer(null)}/>
+          <Progress
+            key={activeQuestionIndex} // key를 index값으로 변경 : 해당 컴포넌트가 재랜더링됨.
+            timeout={10000}
+            onTimeout={() => handleSelectAnswer(null)}
+          />
           <div className="flex justify-between items-center mt-[3vh]">
             <h1 className="sm:text-sm md:text-2xl lg:text-3xl 2xl:text-4xl font-[NanumSquareRoundB]">
-              {`Q${activeQuestionIndex+1}. ${QUSETIONS[activeQuestionIndex].text}`}
+              {`Q${activeQuestionIndex + 1}. ${QUSETIONS[activeQuestionIndex].text}`}
             </h1>
             <button className="flex justify-between items-center rounded-2xl py-1 px-3 hover:bg-gray-200">
               <p className="sm:text-xs md:text-xl 2xl:text-2xl font-[NanumSquareRoundB]">Skip</p>
@@ -58,7 +59,7 @@ function GestureQuiz() {
           </div>
           <div className="flex justify-center w-full h-2/7 bg-white rounded-xl drop-shadow-quiz-box  my-[3vh]">
             {/* 추후, 백앤드에서 blender 애니메이션을 가져올 예정 */}
-              <img src={QUSETIONS[activeQuestionIndex].image} alt="sample_img" className="p-5" />   
+            <img src={QUSETIONS[activeQuestionIndex].image} alt="sample_img" className="p-5" />
           </div>
           {/* 퀴즈 보기 부분 */}
           {/* 퀴즈 내용 백앤드에서 받아와서 보여지도록 하기 */}
@@ -68,12 +69,15 @@ function GestureQuiz() {
             {shuffledAnswers.map((answer, index) => {
               const numberIcons = ['①', '②', '③', '④'];
               return (
-                  <div key={index} className="flex justify-around w-full mt-[3vh]">
-                    <button className="flex justify-center items-center w-2/5 h-[10vh] bg-white rounded-xl drop-shadow-quiz-box hover:bg-[var(--color-kr-500)] hover:text-white sm:text-sm md:text-3xl lg:text-4xl font-[NanumSquareRoundB]" onClick={() => handleSelectAnswer(answer)}>
-                      <p className="mr-5">{numberIcons[index]}</p>
-                      <p>{answer}</p>
-                    </button>
-                  </div>
+                <div key={index} className="flex justify-around w-full mt-[3vh]">
+                  <button
+                    className="flex justify-center items-center w-2/5 h-[10vh] bg-white rounded-xl drop-shadow-quiz-box hover:bg-[var(--color-kr-500)] hover:text-white sm:text-sm md:text-3xl lg:text-4xl font-[NanumSquareRoundB]"
+                    onClick={() => handleSelectAnswer(answer)}
+                  >
+                    <p className="mr-5">{numberIcons[index]}</p>
+                    <p>{answer}</p>
+                  </button>
+                </div>
               );
             })}
           </div>
