aefe10fe1e846e65335f064578a08fb6178306dd
윤지원 <dusqha6731@naver.com>
2025-03-27 20:08:04 +0900

    [FE] chore: env.d.ts 파일 수정
    환경변수 타입 설정
    
    ref: S12P21D203-213

--- FE/src/vite-env.d.ts
+++ FE/src/vite-env.d.ts
@@ -1,1 +1,8 @@
-/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string
+  // 다른 환경변수는 필요에 따라 추가합니다
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
