fa840e33df5eafdd65c9774b909aaaf53f3dfc3c
정주은 <jooeun0807@naver.com>
2025-03-28 15:44:44 +0900

    [FE] feat: 국가별 제스처 목록 조회 API 추가
    - 국가별 제스처 목록 조회 함수 구현
    - API 응답 데이터를 카멜케이스로 변환하는 로직 추가
    
    ref: S12P21D203-214

--- FE/src/services/dictionaryService.ts
+++ FE/src/services/dictionaryService.ts
@@ -1,3 +1,28 @@
+import { GestureResponse } from '../types/dictionaryType';
 import apiClient from '@/api/apiClient';
 
-// 나라별 제스처 목록 조회
+/**
+ * 국가별 제스처 목록 조회
+ * @param countryId
+ * @returns 제스처 목록
+ */
+export const getGesturesByCountry = async (countryId: number) => {
+  const { data } = await apiClient.get<GestureResponse>('/api/gestures/', {
+    params: {
+      country_id: countryId,
+    },
+  });
+
+  // 카멜케이스로 변환
+  return {
+    countryId: data.data.country_id,
+    countryName: data.data.country_name,
+    imageUrl: data.data.image_url,
+    gestures: data.data.gestures.map((gesture) => ({
+      meaningId: gesture.meaning_id,
+      gestureId: gesture.gesture_id,
+      imageUrl: gesture.image_url,
+      gestureTitle: gesture.gesture_title,
+    })),
+  };
+};
