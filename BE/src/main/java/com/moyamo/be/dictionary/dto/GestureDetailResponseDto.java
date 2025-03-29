0ea173dc4b51eff51cf984b4545d8e5fe407cd15
김선주 <sunju701@gmail.com>
2025-03-30 01:19:50 +0900

    [BE] 제스처 정보 상세조회

---
+++ BE/src/main/java/com/moyamo/be/dictionary/dto/GestureDetailResponseDto.java
@@ -0,0 +1,28 @@
+package com.moyamo.be.dictionary.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GestureDetailResponseDto {
+    private int country_id;
+    private String country_name;
+    private String image_url;
+
+    private int meaning_id;
+    private int gesture_id;
+    private String gesture_image;
+    private String gesture_title;
+    private String gesture_meaning;
+    private String gesture_situation;
+    private String gesture_others;
+    private String gesture_tmi;
+
+    @JsonProperty("is_positive")
+    private boolean is_positive;
+    private int multiple_gestures;
+}
