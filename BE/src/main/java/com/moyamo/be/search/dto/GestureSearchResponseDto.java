1211b93ecb822343562b781a437c6ba9097d2389
김민재 <ourtrip@knu.ac.kr>
2025-03-31 09:55:14 +0900

    style: 검색 dto 변수명 변경

--- BE/src/main/java/com/moyamo/be/search/dto/GestureSearchResponseDto.java
+++ BE/src/main/java/com/moyamo/be/search/dto/GestureSearchResponseDto.java
@@ -8,16 +8,16 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 public class GestureSearchResponseDto {
-    private int gestureId;
-    private String gestureName;
-    private String gestureImage;
+    private int gesture_id;
+    private String gesture_name;
+    private String gesture_image;
     private List<Meaning> meanings;
 
     @Getter
     @AllArgsConstructor
     public static class Meaning {
-        private int countryId;
-        private String countryName;
+        private int country_id;
+        private String country_name;
         private String meaning;
     }
 }
