f35e69c2f376044a9d7efe027bbafbc21da3d873
김선주 <sunju701@gmail.com>
2025-03-31 09:42:58 +0900

    [BE] set WebConfig

---
+++ BE/src/main/java/com/moyamo/be/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.moyamo.be.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173", "https://moyamo.site")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
