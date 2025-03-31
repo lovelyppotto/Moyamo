bc69b01691369a2a4ac102e649420fcd5b37334c
윤지원 <dusqha6731@naver.com>
2025-04-01 02:48:07 +0900

    [FE] feat: 미디어파이프 교체
    기존 카메라 컴포넌트 미디어파이프로 교체
    
    ref: S12P21D203-255

--- FE/src/vite-env.d.ts
+++ FE/src/vite-env.d.ts
@@ -2,6 +2,7 @@ interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string
   readonly MODE: string;
   readonly VITE_USE_MOCK_DATA: string;
+  readonly VITE_SERVER_WS_URL: string;
 }
 
 interface ImportMeta {
