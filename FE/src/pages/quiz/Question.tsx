18ce389b33d957611d5eee5db1c7c8822acc87a9
joehyejeong <gpwjdwngml@naver.com>
2025-04-01 17:49:42 +0900

    [FE]feat: 다크모드 적용 - Quiz Start페이지 적용 - Quiz 페이지 적용
    - resolves: S12P21D203-173

--- FE/src/pages/quiz/Question.tsx
+++ FE/src/pages/quiz/Question.tsx
@@ -84,24 +84,23 @@ function Question({ onSelectAnswer, Index, questionData }: ResultProps): JSX.Ele
       <Animation showCorrectImage={showCorrectImage} showWrongImage={showWrongImage} />
       {/* 진행 바, 퀴즈박스 */}
       {/* 마진 탑(margin-top)을 뷰포트 높이(viewport height)의 5%로 설정 */}
-      <div className="h-screen flex flex-col mt-[5vh] xl:mt-[10vh] mx-[2vh] xl:mx-[10vh]">
+      <div className="h-screen flex flex-col mt-[5vh] xl:mt-[10vh] mx-[2vh] xl:mx-[10vh] bg-transparent">
         <div className="flex justfy-center items-center align-middle">
           <PbNumber Index={Index} />
         </div>
         {/* 문제: 계속 이전의 progress값이 저장이 된다 */}
         <Progress
-          // key={activeQuestionIndex} // key를 index값으로 변경 : 해당 컴포넌트가 재랜더링됨.
           key={timer}
           timeout={timer}
           onTimeout={handleSkipAnswer}
           className={progressClass}
         />
         <div className="flex justify-between items-center mt-[3vh]">
-          <h1 className="sm:text-sm md:text-2xl lg:text-3xl 2xl:text-4xl font-[NanumSquareRoundB] mx-[2%]">
+          <h1 className="sm:text-sm md:text-2xl lg:text-3xl 2xl:text-4xl font-[NanumSquareRoundB] mx-[2%] dark:text-white">
             {`Q${Index + 1}. ${questionData.text}`}
           </h1>
           <button
-            className="flex justify-between items-center rounded-2xl py-1 px-3 hover:bg-gray-200 cursor-pointer "
+            className="flex justify-between items-center rounded-2xl py-1 px-3 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer dark:text-white"
             onClick={handleSkipAnswer}
           >
             <p className="sm:text-xs md:text-xl 2xl:text-2xl font-[NanumSquareRoundB]">Skip</p>
