622cb31a8ce12fbaea4c3840e24786930cffbf5a
joehyejeong <gpwjdwngml@naver.com>
2025-04-04 05:43:18 +0900

    [FE]fix: 캐싱 문제 해결 - staleTime: 0 만 사용
    - resolves: S12P21D203-300

--- FE/src/hooks/apiHooks.ts
+++ FE/src/hooks/apiHooks.ts
@@ -98,9 +98,6 @@ export function useQuizQuestions(useCamera: boolean) {
     queryKey: ['quizQuestions', useCamera],
     queryFn: () => getQuizQuestions(useCamera),
     staleTime: 0, // 항상 새로운 데이터를 가져오도록 설정
-    cacheTime: 0, // 캐시를 바로 제거
-    refetchOnMount: true, // 컴포넌트가 마운트될 때마다 새로운 데이터 가져오기
-    refetchOnWindowFocus: false, // 윈도우 포커스 시 자동 리페치 비활성화
   });
 }
 
