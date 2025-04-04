38106c48975d5976ae8070613d382adf9213593f
윤지원 <dusqha6731@naver.com>
2025-04-04 09:32:54 +0900

    [FE] design: 홈 텍스트 크기 수정
    메인 텍스트 크기 수정
    
    ref: S12P21D203-312

--- FE/src/pages/home/Home.tsx
+++ FE/src/pages/home/Home.tsx
@@ -30,31 +30,31 @@ function Home() {
           text-3xl text-center "
         >
           {/* 첫 번째 줄 */}
-          <h1 className="text-2xl sm:text-4xl md:text-5xl xl:text-6xl mb-1 sm:mb-3 md:mb-4">Unlock the world</h1>
+          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl mb-1 sm:mb-3">Unlock the world</h1>
 
           {/* 두 번째 줄 */}
-          <div className="flex items-center justify-center mb-1 sm:mb-3 md:mb-4 w-full">
-            <h1 className="text-2xl sm:text-4xl md:text-5xl xl:text-6xl">through</h1>
+          <div className="flex items-center justify-center mb-1 sm:mb-3 w-full">
+            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl">through</h1>
             <img
               src={getIconImage('puzzle')}
               alt="pixel-puzzle"
               className="w-10 h-10 md:w-14 md:h-14 
               lg:w-16 lg:h-16 xl:w-20 xl:h-20 mx-1 sm:mx-2"
             />
-            <h1 className="dark:text-d-txt-50 text-2xl sm:text-4xl md:text-5xl xl:text-6xl">meaningful</h1>
+            <h1 className="dark:text-d-txt-50 text-2xl sm:text-3xl md:text-4xl lg:text-5xl">meaningful</h1>
           </div>
 
           {/* 세 번째 줄 */}
-          <div className="flex justify-center w-full sm:mb-3 md:mb-6">
+          <div className="flex justify-center w-full sm:mb-3 md:mb-4">
             <div className="relative inline-flex">
               <div
                 className="flex bg-blue-300/60 dark:bg-blue-200 rounded-l-lg
-                px-3 pt-1 pb-1
-                sm:px-4 sm:pt-2 sm:pb-3
-                md:px-5 md:pt-2 md:pb-3 
-                lg:px-6 lg:pt-3 lg:pb-4"
+                px-3 pt-2 pb-1
+                sm:px-4 sm:pt-3 sm:pb-2
+                md:px-5 md:pt-4
+                lg:px-6"
               >
-                <h1 className="dark:text-black text-2xl sm:text-4xl md:text-5xl xl:text-6xl mb-1">gestures</h1>
+                <h1 className="dark:text-black text-2xl sm:text-3xl md:text-4xl lg:text-5xl mb-1">gestures</h1>
               </div>
               {/* 오른쪽 태그 장식 */}
               <div className="flex">
@@ -68,7 +68,7 @@ function Home() {
           </div>
         </div>
         <p
-          className="text-sm sm:text-xl md:text-2xl
+          className="text-sm sm:text-lg md:text-xl
           text-center font-[NanumSquareRoundB]
           mt-3 md:mt-1"
         >
