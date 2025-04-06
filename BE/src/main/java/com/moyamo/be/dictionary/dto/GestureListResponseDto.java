80ff6cac67949240264494a8784c16cd8fb4056c
김선주 <sunju701@gmail.com>
2025-04-06 18:36:09 +0900

    [BE] 국가별 제스처 조회 gesture_type 추가

--- BE/src/main/java/com/moyamo/be/dictionary/dto/GestureListResponseDto.java
+++ BE/src/main/java/com/moyamo/be/dictionary/dto/GestureListResponseDto.java
@@ -1,5 +1,6 @@
 package com.moyamo.be.dictionary.dto;
 
+import com.moyamo.be.dictionary.entity.GestureType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,5 +13,6 @@ public class GestureListResponseDto {
     private String image_url;
     private String gesture_title;
     private String gesture_label;
+    private GestureType gesture_type;
     private int multiple_gestures;
 }
