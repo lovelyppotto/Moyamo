d3d5efc88b47554ffb9eec829286e67f839ff4ca
윤지원 <dusqha6731@naver.com>
2025-04-08 02:12:18 +0900

    [FE] feat: 카메라 모달 안내 문구 수정
    헤더의 안내 문구 수정 및 버튼 문구 변경
    
    ref: S12P21D203-342

--- FE/src/components/cameraModal/CameraDialogHeader.tsx
+++ FE/src/components/cameraModal/CameraDialogHeader.tsx
@@ -3,7 +3,7 @@ import { DialogTitle } from '@/components/ui/dialog';
 function CameraDialogHeader() {
   return (
     <div className="bg-gray-200 dark:bg-gray-700 dark:text-d-txt-50 py-4 px-6">
-      <DialogTitle className="flex item-center text-center text-2xl mb-1 font-[NanumSquareRoundEB]">
+      <DialogTitle className="flex py-1 item-center text-center text-2xl mb-1 font-[NanumSquareRoundEB]">
         제스처 검색
       </DialogTitle>
       <div className="flex flex-col justify-start">
@@ -11,7 +11,10 @@ function CameraDialogHeader() {
           가이드라인에 맞춰 자세를 잡고 카메라 버튼을 누릅니다.
         </p>
         <p className="text-sm text-left font-[NanumSquareRound]">
-          이후 준비 시간 2초가 주어지며, 3초간 제스처를 유지해 주세요.
+          주어지는 2초의 준비 시간 동안 손 전체가 화면에 들어오게 해 주세요.
+        </p>
+        <p className="text-sm text-left font-[NanumSquareRound]">
+          이후 자동으로 3초 동안 제스처 인식이 진행됩니다.
         </p>
       </div>
     </div>
