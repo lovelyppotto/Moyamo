c4f411de7313a1b52761ad5e41f2ef07942017c7
윤지원 <dusqha6731@naver.com>
2025-04-10 20:37:20 +0900

    [FE] feat: pwa 카메라 자동 방향전환 수정
    문제 해결 위한 Screen Orientation API 호출 로직 추가
    
    ref: S12P21D203-290

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
-        orientation: 'portrait',
+        orientation: 'any',
         start_url: '/', // start_url
         icons: [
           {
